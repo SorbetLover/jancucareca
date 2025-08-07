3c6bc1981078d2bde879f2e9c982dea3e98a9f9e
umc4r4aa3 <89319609+SorbetLover@users.noreply.github.com>
2025-08-07 09:27:45 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let juncu
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(displayWidth, displayHeight);
   juncu = loadImage("da70793080916aa33d313bcc5b427d7e.png")
 }
 let merda = [0,0]
@@ -8,8 +8,8 @@ let jumento = 0
 function draw() {
   jumento += 0.08;
   background(220);
-  merda[0] = (400-(265/2)) + 20 * Math. cos(jumento);
-  merda[1] = (300-(257/2)) + 20 * Math. sin(jumento);
+  merda[0] = (displayWidth/2-(265/2)) + 20 * Math. cos(jumento);
+  merda[1] = (displayHeight/2-(257/2)) + 20 * Math. sin(jumento);
   text("jancu careca 😍", merda[0] + 100, merda[1] + 280)
   image(juncu, merda[0], merda[1])
   
