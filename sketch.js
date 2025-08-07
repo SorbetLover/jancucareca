d993b6a0553a555c84f8350545e3544f0178003c
umc4r4aa3 <89319609+SorbetLover@users.noreply.github.com>
2025-08-07 10:45:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,28 @@
-let juncu
+let juncu;
+let merda = [0, 0];
+let jumento = 0;
+
+function preload() {
+  // Carregar imagem no preload (melhor prática)
+  juncu = loadImage("da70793080916aa33d313bcc5b427d7e.png");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  juncu = loadImage("da70793080916aa33d313bcc5b427d7e.png")
+  noScroll(); // remove scroll
 }
-let merda = [0,0]
-let jumento = 0
+
 function draw() {
   jumento += 0.08;
   background(220);
-  merda[0] = (windowWidth/2-(265/2)) + 20 * Math. cos(jumento);
-  merda[1] = (windowHeight/2-(257/2)) + 20 * Math. sin(jumento);
-  text("jancu careca 😍", merda[0] + 100, merda[1] + 280)
-  image(juncu, merda[0], merda[1])
-  
+
+  merda[0] = (windowWidth / 2 - 265 / 2) + 20 * Math.cos(jumento);
+  merda[1] = (windowHeight / 2 - 257 / 2) + 20 * Math.sin(jumento);
+
+  image(juncu, merda[0], merda[1]);
+  text("jancu careca 😍", merda[0] + 100, merda[1] + 280);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
